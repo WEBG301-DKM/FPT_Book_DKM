9c5367e55b5652395e7d8b400159c32564f9d8dc
GWMinhTHD <minhthdgcd210662@fpt.edu.vn>
2023-12-05 14:53:26 +0700

    Inital models creation

---
+++ BookShop1Asm/BookShop1Asm/Data/AppDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BookShop1Asm.Models;
+
+namespace BookShopAsm.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public DbSet<Book> Book{ get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Author> Author { get; set; }
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ BookShop1Asm/BookShop1Asm/Models/Author.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookShop1Asm.Models
+{
+    public class Author
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Alias { get; set; }
+        public string? Photo { get; set; }
+        public string Info { get; set; }
+    }
+}
