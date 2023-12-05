b341e3885387026388ab43f30315355b37912acf
GWMinhTHD <minhthdgcd210662@fpt.edu.vn>
2023-12-05 17:45:24 +0700

    Basic CRUD for Book and Category with img upload

---
+++ BookShop1Asm/BookShop1Asm/Controllers/BookController.cs
@@ -0,0 +1,113 @@
+﻿using BookShop1Asm.Models;
+using BookShopAsm.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop1Asm.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly AppDBContext _dbContext;
+        //private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHost;
+        
+        public BookController(AppDBContext dbContext /*IUnitOfWork unitOfWork*/, IWebHostEnvironment webhost)
+        {
+            _dbContext = dbContext;
+            //_unitOfWork = unitOfWork;
+            _webHost = webhost;
+        }
+
+        public IActionResult Index()
+        {
+            List<Book> books = _dbContext.Book.ToList();
+            //List<Book> books = _unitOfWork.BookRepository.GetAll("Category").ToList();
+            return View(books);
+        }
+
+        public IActionResult CreateUpdate(int? id)
+        {
+            Book book = new Book();
+            if (id == null || id == 0)
+            {
+                //Create new Book
+                return View(book);
+            }
+            else
+            {
+                //Update a Book
+                book = _dbContext.Book.Find(id);
+                return View(book);
+            }
+
+        }
+        [HttpPost]
+
+        public IActionResult CreateUpdate(Book book, IFormFile? file)
+        {
+
+            if (ModelState.IsValid)
+            {
+                string wwwRootPath = _webHost.WebRootPath;
+                if (file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string bookPath = Path.Combine(wwwRootPath, "img\\bookcover");
+                    if (!String.IsNullOrEmpty(book.Cover))
+                    {
+                        var oldImagePath = Path.Combine(wwwRootPath, book.Cover.TrimStart('\\'));
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+                    using (var fileStream = new FileStream(Path.Combine(bookPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+                    book.Cover = @"\img\bookcover\" + fileName;
+                }
+                if (book.Id == 0)
+                {
+                    _dbContext.Book.Add(book);
+                    TempData["success"] = "Book created succesfully";
+                }
+                else
+                {
+                    _dbContext.Book.Update(book);
+                    TempData["success"] = "Book updated succesfully";
+                }
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Book? book = _dbContext.Book.Find(id);
+            //Book? book = _unitOfWork.BookRepository.Get(book => book.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+        [HttpPost]
+        public IActionResult Delete(Book book)
+        {
+
+            _dbContext.Book.Remove(book);
+            _dbContext.SaveChanges();
+            //_unitOfWork.BookRepository.Remove(book);
+            //_unitOfWork.Save();
+            TempData["success"] = "Book deleted succesfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BookShop1Asm/BookShop1Asm/Controllers/CategoryController.cs
@@ -0,0 +1,92 @@
+﻿using BookShop1Asm.Models;
+using BookShopAsm.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop1Asm.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDBContext _dbContext;
+        public CategoryController(AppDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categories = _dbContext.Category.ToList();
+            return View(categories);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            _dbContext.Category.Add(category);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //Category category = _dbContext.Category.Find(category => category.Id == id);
+            //category = _dbContext.Category.FirstOrDefault(category => category.Id == id);
+            Category category = _dbContext.Category.Find(id);
+            //Category category = _unitOfWork.CategoryRepository.Get(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Category.Update(category);
+                _dbContext.SaveChanges();
+                //_unitOfWork.CategoryRepository.Update(category);
+                //_unitOfWork.Save();
+                TempData["success"] = "Edited successfully";
+                return RedirectToAction("index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = _dbContext.Category.Find(id);
+            //Category? category = _unitOfWork.CategoryRepository.Get(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Category category)
+        {
+
+            _dbContext.Category.Remove(category);
+            _dbContext.SaveChanges();
+            //_unitOfWork.CategoryRepository.Remove(category);
+            //_unitOfWork.Save();
+            TempData["success"] = "Category deleted succesfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
