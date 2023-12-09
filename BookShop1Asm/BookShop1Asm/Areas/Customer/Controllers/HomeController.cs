f1002c689c9a3fd6342e648d76516634b70327f5
GWMinhTHD <minhthdgcd210662@fpt.edu.vn>
2023-12-09 14:39:35 +0700

    Add area for Customer with only Home and Privacy

--- BookShop1Asm/BookShop1Asm/Areas/Customer/Controllers/HomeController.cs
+++ BookShop1Asm/BookShop1Asm/Areas/Customer/Controllers/HomeController.cs
@@ -2,8 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-namespace BookShop1Asm.Controllers
+namespace BookShop1Asm.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
